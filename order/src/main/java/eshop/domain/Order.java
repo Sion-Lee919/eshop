d5951aa7ac9386054472c5c74ab0ba5008912f8a
이시온 <sion9199@gmail.com>
2024-12-17 15:39:39 +0900

    commit

---
+++ order/src/main/java/eshop/domain/Order.java
@@ -0,0 +1,127 @@
+package eshop.domain;
+
+import eshop.OrderApplication;
+import eshop.domain.OrderCancelled;
+import eshop.domain.OrderPlaced;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "Order_table")
+@Data
+//<<< DDD / Aggregate Root
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String userId;
+
+    private Long productId;
+
+    private String productName;
+
+    private Integer qty;
+
+    private String status;
+
+    @PostPersist
+    public void onPostPersist() {}
+
+    @PostUpdate
+    public void onPostUpdate() {
+        OrderPlaced orderPlaced = new OrderPlaced(this);
+        orderPlaced.publishAfterCommit();
+    }
+
+    @PreUpdate
+    public void onPreUpdate() {
+        OrderCancelled orderCancelled = new OrderCancelled(this);
+        orderCancelled.publishAfterCommit();
+    }
+
+    public static OrderRepository repository() {
+        OrderRepository orderRepository = OrderApplication.applicationContext.getBean(
+            OrderRepository.class
+        );
+        return orderRepository;
+    }
+
+    //<<< Clean Arch / Port Method
+    public static void updateStatus(DeliveryCompleted deliveryCompleted) {
+        //implement business logic here:
+
+        /** Example 1:  new item 
+        Order order = new Order();
+        repository().save(order);
+
+        */
+
+        /** Example 2:  finding and process
+        
+        repository().findById(deliveryCompleted.get???()).ifPresent(order->{
+            
+            order // do something
+            repository().save(order);
+
+
+         });
+        */
+
+    }
+
+    //>>> Clean Arch / Port Method
+    //<<< Clean Arch / Port Method
+    public static void updateStatus(DeliverReturned deliverReturned) {
+        //implement business logic here:
+
+        /** Example 1:  new item 
+        Order order = new Order();
+        repository().save(order);
+
+        */
+
+        /** Example 2:  finding and process
+        
+        repository().findById(deliverReturned.get???()).ifPresent(order->{
+            
+            order // do something
+            repository().save(order);
+
+
+         });
+        */
+
+    }
+
+    //>>> Clean Arch / Port Method
+    //<<< Clean Arch / Port Method
+    public static void rejectOrder(SoldOut soldOut) {
+        //implement business logic here:
+
+        /** Example 1:  new item 
+        Order order = new Order();
+        repository().save(order);
+
+        */
+
+        /** Example 2:  finding and process
+        
+        repository().findById(soldOut.get???()).ifPresent(order->{
+            
+            order // do something
+            repository().save(order);
+
+
+         });
+        */
+
+    }
+    //>>> Clean Arch / Port Method
+
+}
+//>>> DDD / Aggregate Root
